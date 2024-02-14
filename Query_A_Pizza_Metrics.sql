d3952f0755745f40c68c3e946c9121ea9dae9a10
Edward <e19931107@gmail.com>
2024-02-14 21:54:48 +0800

    Update Question 1

--- Query_A_Pizza_Metrics.sql
+++ Query_A_Pizza_Metrics.sql
@@ -1,10 +1,53 @@
 -- How many pizzas were ordered?
+SELECT COUNT(*) AS No_of_pizzas_ordered
+FROM pizza_runner.customer_orders;
+
+-- Result:
+| No_of_pizzas_ordered |
+| -------------------- |
+| 14                   |
+
+
 -- How many unique customer orders were made?
+
+-- Result:
+
+
 -- How many successful orders were delivered by each runner?
+
+-- Result:
+
+
 -- How many of each type of pizza was delivered?
+
+-- Result:
+
+
 -- How many Vegetarian and Meatlovers were ordered by each customer?
+
+-- Result:
+
 -- What was the maximum number of pizzas delivered in a single order?
+
+-- Result:
+
+
 -- For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
+
+-- Result:
+
+
 -- How many pizzas were delivered that had both exclusions and extras?
+
+-- Result:
+
+
 -- What was the total volume of pizzas ordered for each hour of the day?
+
+-- Result:
+
+
 -- What was the volume of orders for each day of the week?
+
+-- Result:
+
