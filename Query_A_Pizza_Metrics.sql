bd5aad356f15dad891a02d35f077b577c2b5e7e0
Edward <e19931107@gmail.com>
2024-02-15 20:11:40 +0800

    Update Question number and Question 4

--- Query_A_Pizza_Metrics.sql
+++ Query_A_Pizza_Metrics.sql
@@ -1,4 +1,4 @@
--- How many pizzas were ordered?
+--1. How many pizzas were ordered?
 SELECT COUNT(*) AS No_of_pizzas_ordered
 FROM pizza_runner.customer_orders;
 
@@ -8,7 +8,7 @@ FROM pizza_runner.customer_orders;
 | 14                   |
 
 
--- How many unique customer orders were made?
+--2. How many unique customer orders were made?
 SELECT
 DISTINCT customer_id,
 COUNT(*) AS No_of_pizzas_ordered
@@ -25,7 +25,7 @@ GROUP BY customer_id;
 | 105         | 1                    |
 
 
--- How many successful orders were delivered by each runner?
+--3. How many successful orders were delivered by each runner?
 SELECT
 DISTINCT runner_id,
 COUNT(*) AS delivered_orders
@@ -41,36 +41,48 @@ GROUP BY runner_id;
 | 3         | 1                |
 
 
--- How many of each type of pizza was delivered?
+--4. How many of each type of pizza was delivered?
+SELECT 
+DISTINCT pizza_id,
+COUNT(*) AS No_of_delivered
+FROM pizza_runner.customer_orders c
+LEFT JOIN pizza_runner.runner_orders r
+ON c.order_id = r.order_id
+WHERE pickup_time != 0
+GROUP BY pizza_id;
+
+-- Result:
+| pizza_id | No_of_delivered |
+| -------- | --------------- |
+| 1        | 9               |
+| 2        | 3               |
+
+
+--5. How many Vegetarian and Meatlovers were ordered by each customer?
+
+-- Result:
+
+--6. What was the maximum number of pizzas delivered in a single order?
 
 -- Result:
 
 
--- How many Vegetarian and Meatlovers were ordered by each customer?
-
--- Result:
-
--- What was the maximum number of pizzas delivered in a single order?
+--7. For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
 
 -- Result:
 
 
--- For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
+--8. How many pizzas were delivered that had both exclusions and extras?
 
 -- Result:
 
 
--- How many pizzas were delivered that had both exclusions and extras?
+--9. What was the total volume of pizzas ordered for each hour of the day?
 
 -- Result:
 
 
--- What was the total volume of pizzas ordered for each hour of the day?
-
--- Result:
-
-
--- What was the volume of orders for each day of the week?
+--10. What was the volume of orders for each day of the week?
 
 -- Result:
 
