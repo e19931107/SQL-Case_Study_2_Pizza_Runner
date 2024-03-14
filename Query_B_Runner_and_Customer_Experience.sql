9ae9640882e0dcf1c4fa1b63901886d52c7bb131
Edward <e19931107@gmail.com>
2024-03-14 21:50:14 +0800

    Update Question 1

--- Query_B_Runner_and_Customer_Experience.sql
+++ Query_B_Runner_and_Customer_Experience.sql
@@ -1,6 +1,19 @@
 -- 1. How many runners signed up for each 1 week period? (i.e. week starts 2021-01-01)
 
+SELECT 
+    COUNT(*) AS registration_count,
+    WEEK(registration_date) AS registration_week
+FROM 
+    pizza_runner.runners
+GROUP BY WEEK(registration_date);
+
 -- Result:
+| registration_count | registration_week |
+| ------------------ | ----------------- |
+| 1                  | 0                 |
+| 2                  | 1                 |
+| 1                  | 2                 |
+
 
 -- 2. What was the average time in minutes it took for each runner to arrive at the Pizza Runner HQ to pickup the order?
 
