143494f113d391d688c6b5a6970515af40539d3b
Edward <e19931107@gmail.com>
2024-04-07 16:51:26 +0800

    Update Question 4

--- Query_C_Ingredient_Optimisation.sql
+++ Query_C_Ingredient_Optimisation.sql
@@ -128,6 +128,29 @@ ORDER BY order_id ASC;
 | 10       | 1        | null       | null   | Meatlovers |
 
 -- Meat Lovers - Exclude Beef
+SELECT pc.order_id, pc.pizza_id, pc.exclusions, pc.extras,pp.pizza_name
+FROM pizza_runner.customer_orders pc
+LEFT JOIN pizza_runner.pizza_names pp
+on pc.pizza_id = pp.pizza_id
+WHERE pp.pizza_name = 'Meatlovers'#
+ORDER BY order_id ASC;
+
+-- Result:
+| order_id | pizza_id | exclusions | extras | pizza_name |
+| -------- | -------- | ---------- | ------ | ---------- |
+| 1        | 1        |            |        | Meatlovers |
+| 2        | 1        |            |        | Meatlovers |
+| 3        | 1        |            |        | Meatlovers |
+| 4        | 1        | 4          |        | Meatlovers |
+| 4        | 1        | 4          |        | Meatlovers |
+| 5        | 1        | null       | 1      | Meatlovers |
+| 8        | 1        | null       | null   | Meatlovers |
+| 9        | 1        | 4          | 1, 5   | Meatlovers |
+| 10       | 1        | null       | null   | Meatlovers |
+| 10       | 1        | 2, 6       | 1, 4   | Meatlovers |
+
+
+
 -- Meat Lovers - Extra Bacon
 -- Meat Lovers - Exclude Cheese, Bacon - Extra Mushroom, Peppers
 
